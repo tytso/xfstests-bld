c37241a270b951e66588a78070a38d03afa524ae
Theodore Ts'o <tytso@mit.edu>
2017-08-25 10:49:35 -0400

    test-appliance: drop the use of fluentd for the GCE test appliance
    The fluentd daemon is used to support Stackdriver, which we are
    currently not using.  It also logs all of the syslogs for the test
    VM's in JSON format into the GCS bucket, which can consume quite a lot
    of space.
    
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- kvm-xfstests/test-appliance/gce-xfstests-bld.sh
+++ kvm-xfstests/test-appliance/gce-xfstests-bld.sh
@@ -150,8 +150,6 @@ chmod +rx /usr/local/lib/gce-ltm/app.py
 
 gcloud components -q update
 
-# Install logging agent
-curl https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh | bash
 ZONE=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google")
 ID=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/id" -H "Metadata-Flavor: Google")
 logger -s "xfstests GCE appliance build completed (build instance id $ID)"
