b9699ee3a0f277c62275fced4bfe43c61466b913
Theodore Ts'o <tytso@mit.edu>
2018-08-07 20:54:02 -0400

    build_config.sh: Update xfstests commit
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- build_config.sh
+++ build_config.sh
@@ -1,10 +1,10 @@
 ARCH_LIST=(amd64 i386 arm64 armhf)
-MIRROR=http://snapshot.debian.org/archive/debian/20180505T160451Z/
+MIRROR=http://snapshot.debian.org/archive/debian/20180529T032410Z/
 
 #BEGIN CONFIG.CUSTOM
 
 XFSTESTS_GIT=https://github.com/tytso/xfstests
-XFSTESTS_COMMIT=5cc2363625b752fda09c09903ffa8842af327d40
+XFSTESTS_COMMIT=34977a44c0d813e603c64567cc9d8b1c8ef32edd
 
 XFSPROGS_GIT=https://git.kernel.org/pub/scm/fs/xfs/xfsprogs-dev.git
 XFSPROGS_COMMIT=v4.16.0
@@ -13,16 +13,16 @@ FIO_GIT=http://git.kernel.dk/fio.git
 FIO_COMMIT=fio-3.2
 
 QUOTA_GIT=https://git.kernel.org/pub/scm/utils/quota/quota-tools.git
-QUOTA_COMMIT=59b280e
+QUOTA_COMMIT=59b280ebe22eceaf4250cb3b776674619a4d4ece
 
 # SYZKALLER_GIT=https://github.com/google/syzkaller
-# SYZKALLER_COMMIT=d5a5d045176c3
+# SYZKALLER_COMMIT=2f93b54f26aa40233a0a584ce8714e55c8dd159a
 
 FSVERITY_GIT=git://git.kernel.org/pub/scm/linux/kernel/git/mhalcrow/fsverity.git
-FSVERITY_COMMIT=2a7dbea90885
+FSVERITY_COMMIT=2a7dbea90885dbd1dadc3d4a2873008ae618614e
 
 IMA_EVM_UTILS_GIT=git://git.code.sf.net/p/linux-ima/ima-evm-utils.git
-IMA_EVM_UTILS_COMMIT=v1.1
+IMA_EVM_UTILS_COMMIT=5fa7d35de50c65ac58911ca4f7f0bb8c076d7ecf
 
 #EXEC_LDFLAGS=-static
 #EXEC_LLDFLAGS=-all-static
