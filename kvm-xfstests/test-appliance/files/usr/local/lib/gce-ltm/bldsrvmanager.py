7dcc236df2f3698c257e1ef8c1979cc272ecfb3d
ZS <zpshi@bu.edu>
2019-12-04 00:56:17 -0500

    test-appliance: enabled ltm to send build requests to build server

--- kvm-xfstests/test-appliance/files/usr/local/lib/gce-ltm/bldsrvmanager.py
+++ kvm-xfstests/test-appliance/files/usr/local/lib/gce-ltm/bldsrvmanager.py
@@ -46,6 +46,15 @@ class BldsrvManager(object):
             logging.error('Build server failed to start')
         else:
             logging.info('Successfully launched build server')
+            # waiting for build server to set up
+            for _ in range(120):
+                sleep(1.0)
+            # get build server ip address
+            bldsrv_ip = self.__get_bldsrv_ip()          
+            # login and send cmdline to bldsrv
+            sent = self.__send_to_bldsrv(bldsrv_ip)
+            if sent == 'false':
+                logging.error('Failed to send cmd to build server')
             # self.__monitor()
             # logging.info('Exiting monitor process')
         exit()
@@ -56,6 +65,31 @@ class BldsrvManager(object):
         returned = call(self.launch_bldsrv_cmd)
         logging.info('Command returned %s', returned)
         return returned == 0
+
+    def __get_bldsrv_ip(self):
+        self.compute = googleapiclient.discovery.build('compute','v1')
+        bldsrv_info = self.compute.instances().get(
+                project=self.gce_project, zone=self.gce_zone,
+                instance=self.instance_name).execute()
+        bldsrv_ip = bldsrv_info['networkInterfaces'][-1]['accessConfigs'][0]['natIP']
+        logging.info('Build server ip address: %s', bldsrv_ip)
+        return bldsrv_ip
+
+    def __send_to_bldsrv(self, bldsrv_ip):
+        with open('pwd.json', 'r') as f:
+            pwd = json.load(f)
+        logging.info('Build server password: %s', pwd)
+        logging.info('gce-xfstests original command line: %s', self.cmd_json)
+        header = {'Content-Type': 'application/json'}
+        with requests.Session() as s:
+            url_login = 'https://' + bldsrv_ip + '//login'
+            r = s.post(url_login, json=pwd, headers=header, verify=False)
+            logging.info('log in request return: %s', r.content)
+            url_gce = 'https://' + bldsrv_ip + '//gce-xfstests'
+            r = s.post(url_gce, json=self.cmd_json, headers=header, verify=False)
+            logging.info('gce cmd request return: %s', r.content)
+            logging.info('returned status %s', r.content['status'])
+        return r.content.split('"status":')[1].split('}')[0]
 
     def __monitor(self):
         # logging.info('Entered monitor')
