b8ebbd46475b06312994fe8b63736e07a10d2d04
Theodore Ts'o <tytso@mit.edu>
2020-03-02 14:25:10 -0500

    build_config.sh: update to latest git versions
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- build_config.sh
+++ build_config.sh
@@ -4,13 +4,13 @@ MIRROR=http://snapshot.debian.org/archive/debian/20190826T092742Z/
 #BEGIN CONFIG.CUSTOM
 
 XFSTESTS_GIT=https://github.com/tytso/xfstests
-XFSTESTS_COMMIT=dce1dac57c50ec6442e6353f50153f93e4b5e930
+XFSTESTS_COMMIT=6334dfdaaf8d7682e7e2a93f6f5850c40b604059 # 2020-02-02 release
 
 XFSPROGS_GIT=https://git.kernel.org/pub/scm/fs/xfs/xfsprogs-dev.git
-XFSPROGS_COMMIT=1609c11ae071e7c7b6309bf94f291faf1a2006b3 # v5.3.0
+XFSPROGS_COMMIT=0cfb2952319d237f1d079097810546f24e3883bf # v5.4.0
 
 FIO_GIT=http://git.kernel.dk/fio.git
-FIO_COMMIT=01bf5128d0581e267383f280c6a1dcd26517240f # fio-3.15
+FIO_COMMIT=ac694f66968fe7b18c820468abd8333f3df333fb # fio-3.18
 
 QUOTA_GIT=https://git.kernel.org/pub/scm/utils/quota/quota-tools.git
 QUOTA_COMMIT=9a001cc6eb211758015d85cecc0464c94c82bbb5
@@ -19,16 +19,20 @@ QUOTA_COMMIT=9a001cc6eb211758015d85cecc0464c94c82bbb5
 # SYZKALLER_COMMIT=bab43553a904660266fdcd8fb974c7bdd96b3f58
 
 FSVERITY_GIT=https://git.kernel.org/pub/scm/linux/kernel/git/ebiggers/fsverity-utils.git/
-FSVERITY_COMMIT=6585eb4968a0f3f0811bd8707ff5b04c78cf1c5e
+FSVERITY_COMMIT=6585eb4968a0f3f0811bd8707ff5b04c78cf1c5e # v1.0
 
 IMA_EVM_UTILS_GIT=git://git.code.sf.net/p/linux-ima/ima-evm-utils.git
-IMA_EVM_UTILS_COMMIT=515c99856ef52bbf680e6dd6c338acfb8d088614
+IMA_EVM_UTILS_COMMIT=515c99856ef52bbf680e6dd6c338acfb8d088614 # v1.2
 
 BLKTESTS_GIT=https://github.com/tytso/blktests.git
-BLKTESTS_COMMIT=e86852d221f38536fbb1ee595f2276d8a13d4be3
+BLKTESTS_COMMIT=f7b47c58e87caf91e1648b3c0fa348c555595800
 
 NVME_CLI_GIT=https://github.com/linux-nvme/nvme-cli
-NVME_CLI_COMMIT=b7eb621f1a998f9b7a58501fdf6e6773ddc937ff
+NVME_CLI_COMMIT=1d84d6ae0c7d7ceff5a73fe174dde8b0005f6108 # v1.10.1
+
+UTIL_LINUX_GIT=https://git.kernel.org/pub/scm/utils/util-linux/util-linux.git
+UTIL_LINUX_COMMIT=d35573591278a231a62cefdbee5b0beb0fb62dc8 # v2.35
+UTIL_LINUX_LIBS_ONLY=yes
 
 #EXEC_LDFLAGS=-static
 #EXEC_LLDFLAGS=-all-static
