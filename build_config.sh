f79ed647fdedbbeca9b8394679fda22c7368dde0
Theodore Ts'o <tytso@mit.edu>
2020-03-22 14:46:25 -0400

    build_config.sh: update to newer git versions
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- build_config.sh
+++ build_config.sh
@@ -4,13 +4,13 @@ MIRROR=http://snapshot.debian.org/archive/debian/20190826T092742Z/
 #BEGIN CONFIG.CUSTOM
 
 XFSTESTS_GIT=https://github.com/tytso/xfstests
-XFSTESTS_COMMIT=b15701bcefccc59b519b5a8d26c0bcf07751cb22 # 2020-03-19 release
+XFSTESTS_COMMIT=8c2da11cf6ee2eb09efcc7ac209cfc61d257e75f # 2020-03-22 release
 
 XFSPROGS_GIT=https://git.kernel.org/pub/scm/fs/xfs/xfsprogs-dev.git
-XFSPROGS_COMMIT=0cfb2952319d237f1d079097810546f24e3883bf # v5.4.0
+XFSPROGS_COMMIT=c5f89a875886e90464baee6ed470597e5339b920 # v5.4.0
 
 FIO_GIT=http://git.kernel.dk/fio.git
-FIO_COMMIT=ac694f66968fe7b18c820468abd8333f3df333fb # fio-3.18
+FIO_COMMIT=f75248a9d9554b668484b089713e7c2b0a154ad6 # fio-3.19
 
 QUOTA_GIT=https://git.kernel.org/pub/scm/utils/quota/quota-tools.git
 QUOTA_COMMIT=9a001cc6eb211758015d85cecc0464c94c82bbb5
@@ -25,13 +25,13 @@ IMA_EVM_UTILS_GIT=git://git.code.sf.net/p/linux-ima/ima-evm-utils.git
 IMA_EVM_UTILS_COMMIT=515c99856ef52bbf680e6dd6c338acfb8d088614 # v1.2
 
 BLKTESTS_GIT=https://github.com/tytso/blktests.git
-BLKTESTS_COMMIT=f7b47c58e87caf91e1648b3c0fa348c555595800
+BLKTESTS_COMMIT=cd11d001fe8619f491fcbf89517a14169411ac3e
 
 NVME_CLI_GIT=https://github.com/linux-nvme/nvme-cli
 NVME_CLI_COMMIT=1d84d6ae0c7d7ceff5a73fe174dde8b0005f6108 # v1.10.1
 
 UTIL_LINUX_GIT=https://git.kernel.org/pub/scm/utils/util-linux/util-linux.git
-UTIL_LINUX_COMMIT=d35573591278a231a62cefdbee5b0beb0fb62dc8 # v2.35
+UTIL_LINUX_COMMIT=77efaa9761df88f7bef842ac53ac1e9a1718b67e # v2.35.1
 UTIL_LINUX_LIBS_ONLY=yes
 
 #EXEC_LDFLAGS=-static
