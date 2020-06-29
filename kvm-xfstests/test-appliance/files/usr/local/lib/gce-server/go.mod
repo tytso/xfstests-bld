a3cb16214c3292990e0afc3ca74c9af5a20f6c93
Tim Shen <xyshen@google.com>
2020-06-29 04:34:05 +0000

    ltm go server: add prototype for session and login support
    Use third-party package gorilla to enable user login and sessions.
    Not used in the go server currently but will be integrated later.
    
    Signed-off-by: Tim Shen <xyshen@google.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- kvm-xfstests/test-appliance/files/usr/local/lib/gce-server/go.mod
+++ kvm-xfstests/test-appliance/files/usr/local/lib/gce-server/go.mod
@@ -3,10 +3,12 @@ module example.com/gce-server
 go 1.14
 
 require (
-	cloud.google.com/go v0.58.0 // indirect
-	cloud.google.com/go/storage v1.10.0 // indirect
+	cloud.google.com/go v0.59.0 // indirect
+	cloud.google.com/go/storage v1.10.0
 	github.com/google/uuid v1.1.1
-	golang.org/x/net v0.0.0-20200602114024-627f9648deb9
+	github.com/gorilla/securecookie v1.1.1
+	github.com/gorilla/sessions v1.2.0
+	golang.org/x/net v0.0.0-20200625001655-4c5254603344
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 	google.golang.org/api v0.28.0
 )
--- kvm-xfstests/test-appliance/files/usr/local/lib/gce-server/util/server.go
+++ kvm-xfstests/test-appliance/files/usr/local/lib/gce-server/util/server.go
@@ -2,8 +2,14 @@ package util
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/gorilla/securecookie"
+	"github.com/gorilla/sessions"
 )
+
+const sessionsKeyPath = "/usr/local/lib/gce-server/.sessions_secret_key"
 
 // Options contains configs for web requests in the json POST body.
 type Options struct {
@@ -13,6 +19,11 @@ type Options struct {
 	ReportEmail   string `json:"report_email"`
 	CommitID      string `json:"commit_id"`
 	GitRepo       string `json:"git_repo"`
+}
+
+// LoginRequest contains a password for user authentication
+type LoginRequest struct {
+	Password string `json:"password"`
 }
 
 // UserRequest contains the full cmd from user in base 64 and some configs.
@@ -32,18 +43,51 @@ type MsgResponse struct {
 	Msg    string `json:"message"`
 }
 
+var (
+	key   []byte
+	store *sessions.CookieStore
+)
+
+func init() {
+	if FileExists(sessionsKeyPath) {
+		buf, err := ioutil.ReadFile(sessionsKeyPath)
+		Check(err)
+		key = buf
+	} else {
+		key = securecookie.GenerateRandomKey(32)
+		err := ioutil.WriteFile(sessionsKeyPath, key, 0644)
+		Check(err)
+	}
+	store = sessions.NewCookieStore(key)
+}
+
 // Index handles the root endpoint.
 func Index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 	w.Write([]byte("index page"))
 }
 
-// Login handles the login reguest (deprecated).
+// Login handles the login reguest (not used for now).
 func Login(w http.ResponseWriter, r *http.Request) {
+	var c LoginRequest
+	err := json.NewDecoder(r.Body).Decode(&c)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	session, err := store.Get(r, "single-session")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	session.Values["pwd"] = c.Password
+	err = session.Save(r, w)
+
 	stat := SimpleResponse{true}
 	js, err := json.Marshal(stat)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
