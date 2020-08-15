349da2ab8f90a62aba690e2b517829ff8e9147f3
Tim Shen <xyshen@google.com>
2020-08-15 01:31:10 +0000

    go server bisect: improve git revision parsing logic
    Validate user provided git revisions before starting bisect. Append prefix
    "origin/" to the commit if applicable. Use git clone --shared to create
    the local repos faster.
    
    Signed-off-by: Tim Shen <xyshen@google.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- kvm-xfstests/test-appliance/files/usr/local/lib/gce-server/util/git/git_test.go
+++ kvm-xfstests/test-appliance/files/usr/local/lib/gce-server/util/git/git_test.go
@@ -1,17 +1,87 @@
 package git
 
 import (
-	"os"
+	"gce-server/util/check"
+	"io/ioutil"
 	"testing"
 )
 
-func TestDelete(t *testing.T) {
-	repo, err := NewRepository("test", "https://github.com/tytso/ext4.git", os.Stdout)
+var ext4Url = "https://github.com/tytso/ext4.git"
+
+func TestNewRepository(t *testing.T) {
+	repo, err := NewRepository("test", ext4Url, ioutil.Discard)
 	if err != nil {
 		t.Error(err)
+	}
+	if !check.DirExists(RefRepoDir) {
+		t.Error("reference linux repo not found")
+	}
+	if !check.DirExists(repo.dir) {
+		t.Error("repo dir not found")
 	}
 	err = repo.Delete()
 	if err != nil {
 		t.Error(err)
+	}
+	if check.DirExists(repo.dir) {
+		t.Error("repo dir not deleted")
+	}
+}
+
+func TestCheckout(t *testing.T) {
+	commit := "v5.6"
+	hash := "7111951b8d4973bda27ff663f2cf18b663d15b48"
+
+	repo, err := NewRepository("test", ext4Url, ioutil.Discard)
+	if err != nil {
+		t.Error(err)
+	}
+	err = repo.Checkout(commit, ioutil.Discard)
+	if err != nil {
+		t.Error(err)
+	}
+	head, err := repo.GetCommit(ioutil.Discard)
+	if err != nil {
+		t.Error(err)
+	}
+	if head != hash {
+		t.Errorf("get wrong repo head %s instead of %s", head, hash)
+	}
+	err = repo.Delete()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestNewRemoteRepository(t *testing.T) {
+	hash := "868b66c4a7670ed3c1795cb471974342a369b1e1"
+
+	repo, err := NewRemoteRepository(ext4Url, "bisect-test-generic-307")
+	if err != nil {
+		t.Error(err)
+	}
+	if repo.Head() != hash {
+		t.Errorf("get wrong repo head %s instead of %s", repo.Head(), hash)
+	}
+}
+
+func TestParseURL(t *testing.T) {
+	tests := []struct {
+		url string
+		dir string
+	}{
+		{"https://github.com/XiaoyangShen/spinner_test.git",
+			"github.com-XiaoyangShen-spinner_test.git-381e3d6d"},
+		{"git://git.kernel.org/pub/scm/linux/kernel/git/elder/linux.git",
+			"git.kernel.org-elder-linux.git-7353b8c3"},
+	}
+	for _, e := range tests {
+		dir, err := ParseURL(e.url)
+		if err != nil {
+			t.Error(err)
+		}
+		if dir != e.dir {
+			t.Errorf("get wrong parsed url %s instead of %s", dir, e.dir)
+		}
 	}
 }
