47b5c37c7592a3488e91627813826abc479a8cc9
Theodore Ts'o <tytso@mit.edu>
2021-11-04 22:52:53 -0400

    build_config.sh: update to latest git versions
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- build_config.sh
+++ build_config.sh
@@ -1,37 +1,37 @@
 ARCH_LIST=(amd64 i386 arm64)
-MIRROR=http://snapshot.debian.org/archive/debian/20210411T144547Z/
+MIRROR=https://snapshot.debian.org/archive/debian/20211104T150851Z/
 
 #BEGIN CONFIG.CUSTOM
 
 XFSTESTS_GIT=https://github.com/tytso/xfstests
-XFSTESTS_COMMIT=6aa18fb84823a83040719b6490c93910d1e22196 # 2021-07-02 release
+XFSTESTS_COMMIT=df8f6dbc5d981d6a9dddeaa4841d33c1a0fbf3fa # 2021-11-04 release
 
 XFSPROGS_GIT=https://git.kernel.org/pub/scm/fs/xfs/xfsprogs-dev.git
-XFSPROGS_COMMIT=3e384caad5663aed3071a1dff3da85b9ab5129dc # v5.12.0
+XFSPROGS_COMMIT=b42033308360655616fc9bd77678c46bf518b7c8 # v5.13.0
 
 FIO_GIT=http://git.kernel.dk/fio.git
-FIO_COMMIT=0313e938c9c8bb37d71dade239f1f5326677b079 # fio-3.27
+FIO_COMMIT=d83ac9d3d30d5f5dc6d0e425e4ba945a772839f6 # fio-3.28
 
 QUOTA_GIT=https://git.kernel.org/pub/scm/utils/quota/quota-tools.git
-QUOTA_COMMIT=25f16b1de313ce0d411f754572f94f051bfbe3c8
+QUOTA_COMMIT=d2256ac2d44b0a5be9c0b49ce4ce8e5f6821ce2a
 
 # SYZKALLER_GIT=https://github.com/google/syzkaller
 # SYZKALLER_COMMIT=bab43553a904660266fdcd8fb974c7bdd96b3f58
 
 FSVERITY_GIT=https://git.kernel.org/pub/scm/linux/kernel/git/ebiggers/fsverity-utils.git/
-FSVERITY_COMMIT=9e082897d61a2449657651aa5a0931aca31428fd # v1.4
+FSVERITY_COMMIT=ddc6bc9daeb79db932aa12edb85c7c2f4647472a # v1.4-4-gddc6bc9
 
 IMA_EVM_UTILS_GIT=git://git.code.sf.net/p/linux-ima/ima-evm-utils.git
 IMA_EVM_UTILS_COMMIT=00a0e66a14d3663edd9d37c8a01db6d182c88bdd # v1.3.2
 
 BLKTESTS_GIT=https://github.com/tytso/blktests.git
-BLKTESTS_COMMIT=62ec9d600e2a2cfe5589fc4343eede02b90a2555
+BLKTESTS_COMMIT=3be78490def51cc477a659412a8b369af434bddc
 
 NVME_CLI_GIT=https://github.com/linux-nvme/nvme-cli
-NVME_CLI_COMMIT=f729e93e2c08ed87ea0007238c729dbf911cd433 # v1.14-61-gf729e93
+NVME_CLI_COMMIT=00e93879605449cabd525a85b1f8cd467e224aca # v1.15
 
 UTIL_LINUX_GIT=https://git.kernel.org/pub/scm/utils/util-linux/util-linux.git
-UTIL_LINUX_COMMIT=50736e4998fde0fff9b7876476137a21b85bd5a6 # v2.37
+UTIL_LINUX_COMMIT=f59c8fd38dfee24b93ed54a6984f879499c34ec7 # v2.37.2
 UTIL_LINUX_LIBS_ONLY=yes
 
 #EXEC_LDFLAGS=-static
