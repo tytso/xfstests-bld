40d509b026e314ee72895fbd8d9288fe2ac18722
Theodore Ts'o <tytso@mit.edu>
2023-03-04 18:37:41 -0500

    build_config.sh: update to latest git versions
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- build_config.sh
+++ build_config.sh
@@ -5,28 +5,28 @@ DISTRO=bullseye
 #BEGIN CONFIG.CUSTOM
 
 XFSTESTS_GIT=https://github.com/tytso/xfstests
-XFSTESTS_COMMIT=289f50f8b30a5372ab66d293df5563efc7cf040b # 2022-08-22 release
+XFSTESTS_COMMIT=821ef4889cb6a568a237a52ca0eee0332188d049 # release-2023-03-03-821ef4889
 
 XFSPROGS_GIT=https://git.kernel.org/pub/scm/fs/xfs/xfsprogs-dev.git
-XFSPROGS_COMMIT=5652dc4fbb6c9e7a4911ddcc4c3e373a4d014a6f # v5.19.0
+XFSPROGS_COMMIT=fdf0366959f1d04f2aea93a3fac24c49b9d5e55f # v6.1.1
 
 FIO_GIT=http://git.kernel.dk/fio.git
 FIO_COMMIT=6cafe8445fd1e04e5f7d67bbc73029a538d1b253 # fio-3.31
 
 QUOTA_GIT=https://git.kernel.org/pub/scm/utils/quota/quota-tools.git
-QUOTA_COMMIT=d2256ac2d44b0a5be9c0b49ce4ce8e5f6821ce2a
+QUOTA_COMMIT=d90b7d585067e87c56d8462b8e3e1c68996e2fc1
 
 # SYZKALLER_GIT=https://github.com/google/syzkaller
 # SYZKALLER_COMMIT=bab43553a904660266fdcd8fb974c7bdd96b3f58
 
-FSVERITY_GIT=https://git.kernel.org/pub/scm/linux/kernel/git/ebiggers/fsverity-utils.git/
-FSVERITY_COMMIT=20e87c13075a8e5660a8d69fd6c93d4f7c5f01a5 # v1.5
+FSVERITY_GIT=https://git.kernel.org/pub/scm/fs/fsverity/fsverity-utils.git
+FSVERITY_COMMIT=5d6f7c4c2f82140207cf0300683217efe6cd0daa
 
 IMA_EVM_UTILS_GIT=git://git.code.sf.net/p/linux-ima/ima-evm-utils.git
 IMA_EVM_UTILS_COMMIT=00a0e66a14d3663edd9d37c8a01db6d182c88bdd # v1.3.2
 
 BLKTESTS_GIT=https://github.com/tytso/blktests.git
-BLKTESTS_COMMIT=4e07b0c360db5e74d3cbe5b3c6c8b86199017fbf
+BLKTESTS_COMMIT=676d42c9f932f4b6ca3f25439c2fc09d595ceefd
 
 NVME_CLI_GIT=https://github.com/linux-nvme/nvme-cli
 NVME_CLI_COMMIT=deee9cae1ac94760deebd71f8e5449061338666c # v1.16
